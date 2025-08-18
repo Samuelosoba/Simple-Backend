9cb0ee335409ecf03435845bc4bcad1437b1e84f
osobasamuel <samuelosoba7@gmail.com>
2025-08-18 20:11:12 +0100

    .

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -17,3 +17,27 @@ export const getTasks = async (req, res, next) => {
     next(err);
   }
 };
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found" });
+    }
+
+   
+    if (task.user.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Not authorized" });
+    }
+
+    await task.remove();
+
+    res.status(200).json({ success: true, message: "Task deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
--- src/routes/task.js
+++ src/routes/task.js
@@ -1,10 +1,11 @@
 // routes/taskRoutes.js
 import express from "express";
-import { createTask, getTasks } from "../controllers/task.js";
+import { createTask, deleteTask, getTasks } from "../controllers/task.js";
 import { verifyToken, isAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 router.get("/", getTasks);
-router.post("/", verifyToken, createTask); // only admin
+router.post("/", verifyToken, createTask);
+router.delete("/:id", verifyToken, deleteTask);
 
 export default router;
